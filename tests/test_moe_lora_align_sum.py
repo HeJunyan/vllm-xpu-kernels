391de5cb44504b2e66e41cf778e8b255c4b73661
Ma, Liangliang <liangliang.ma@intel.com>
2026-04-14 13:58:57 +0800

    Revert "[MoE][SYCL] skip test_moe_lora_align_sum.py for op not used in PO (#59)"
    This reverts commit d414fce8bfc4f8f16ffaee9dbaf55455b11b65ec.

--- tests/test_moe_lora_align_sum.py
+++ tests/test_moe_lora_align_sum.py
@@ -7,8 +7,6 @@ import torch
 
 import tests.register_ops as ops
 
-pytestmark = pytest.mark.skip(reason="Skip for PO/MOE not use this op currently")
-
 # override pytest parameters when enable mini pytest
 MINI_PYTEST_PARAMS = {
     "default": {
