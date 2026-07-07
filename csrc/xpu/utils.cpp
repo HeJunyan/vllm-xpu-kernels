52828c56874b546bce2c81920e417b8052f115cc
zofia <110436990+zufangzhu@users.noreply.github.com>
2026-07-07 18:45:17 +0800

    add api: is_xe2_arch (#450)
    * add api is_xe2_arch
    
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>
    
    * add api is_xe3_arch
    
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>
    
    * format
    
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>
    
    ---------
    
    Signed-off-by: Zhu, Zufang <zufang.zhu@intel.com>
    (cherry picked from commit 2e2d56e5b59e147d06bc617d2ca883a8795706ab)

--- csrc/xpu/utils.cpp
+++ csrc/xpu/utils.cpp
@@ -31,3 +31,13 @@ bool is_nvl_p(int64_t device_index = -1) {
   at::DeviceIndex dev_idx = device_index;
   return vllm::xpu::is_nvl_p(dev_idx);
 }
+
+bool is_xe2_arch(int64_t device_index = -1) {
+  at::DeviceIndex dev_idx = device_index;
+  return vllm::xpu::is_xe2_arch(dev_idx);
+}
+
+bool is_xe3_arch(int64_t device_index = -1) {
+  at::DeviceIndex dev_idx = device_index;
+  return vllm::xpu::is_xe3_arch(dev_idx);
+}
