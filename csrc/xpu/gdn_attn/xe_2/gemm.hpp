92ed417e75920fc7d2e09dc6a40a7a4b7a2d8269
Ma, Liangliang <liangliang.ma@intel.com>
2026-08-07 23:55:08 +0800

    fix build: use SPIRVScope for barrier_scope in gemm_TTS_fused_2A
    The GDN split (upstream #402) added gemm_TTS_fused_2A with the legacy
    'constexpr int barrier_scope = 2' form. The cutlass-sycl revision pinned
    by this repo requires cute::barrier_arrive/barrier_wait to take an
    SPIRVScope, matching the other four gemm_* helpers in this file.
    
    Signed-off-by: Ma, Liangliang <liangliang.ma@intel.com>

--- csrc/xpu/gdn_attn/xe_2/gemm.hpp
+++ csrc/xpu/gdn_attn/xe_2/gemm.hpp
@@ -450,7 +450,7 @@ CUTE_DEVICE void gemm_TTS_fused_2A(
 
   const int prefetch_dist = 3;
 
-  constexpr int barrier_scope = 2;
+  constexpr SPIRVScope barrier_scope = ScopeWorkgroup;
 
   int k_tile_count = ceil_div(shape<1>(A1), get<2>(wg_tile));
   int k_tile_prefetch = 0;
